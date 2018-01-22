0512baec8feefa119a0e3de2278926c5de2ca4c6
Sunny Kotwal <sunnykotwal@yahoo.com>
2018-01-22 05:07:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ function johnLennonFacts(facts) {
  }
  
  function iLoveTheBeatles(number) {
-   var array = []
+   var array = [] ;
    do { 
-     array.push("I love the Beatles!")
+     array.push("I love the Beatles!");
      number++
   } while 
   number(< 15)
