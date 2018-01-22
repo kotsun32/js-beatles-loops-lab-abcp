818c72baa6a5520b44d23ff46b2173dafa207e6e
Sunny Kotwal <sunnykotwal@yahoo.com>
2018-01-22 04:58:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ function theBeatlesPlay(musicans, instruments) {
     
 }
 
-function johnLennonFacts() {
-   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+
+function johnLennonFacts(facts) {
+   
   var array = [];
    var i = 0;
    while (array.length < facts.length) {
